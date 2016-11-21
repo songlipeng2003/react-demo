cfdbea9f638bb3371253371a5cb65a62874af553
Thinking Song <123wuming_123@163.com>
2016-11-21 17:59:30 +0800

    add react-router-redux

--- src/index.js
+++ src/index.js
@@ -4,21 +4,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import demoApp from './reducers'
-
-const store = createStore(demoApp);
+import account from './reducers/account'
 
 import App from './components/App';
 import Main from './components/Main';
 import TopicPage from './components/TopicPage';
 import LoginPage from './components/LoginPage';
 
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+
+const store = createStore(
+  combineReducers({
+    account: account,
+    routing: routerReducer
+  })
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
+
 // Render the main component into the dom
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={history}>
       <Route path="/" component={App}>
         <IndexRoute component={Main}/>
         <Route path="topic/:id" component={TopicPage} />
