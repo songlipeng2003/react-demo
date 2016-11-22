f5c1a7af3c5734446998e8fb7fafe42bd184af79
Thinking Song <123wuming_123@163.com>
2016-11-22 17:03:01 +0800

    add show my info

--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -1,24 +1,42 @@
 import React, { PropTypes } from 'react';
 
 import { NavBar } from 'antd-mobile';
+
 import { connect } from 'react-redux'
 import { browserHistory } from 'react-router'
+
+import { User } from '../api';
 
 class MyPage extends React.Component {
   constructor(props){
     super(props);
+
+    this.state = {
+      user: {}
+    };
   }
 
   componentDidMount(){
     if(!this.props.account.logined){
       browserHistory.push('/login');
     }
+
+    User.get(this.props.account.loginname).then((response) => {
+      this.setState({
+        user: response.data.data
+      });
+    });
   }
 
   render() {
     return (
-      <div>
+      <div className="my">
         <NavBar>我的</NavBar>
+
+        <div className="info">
+          <img src={this.state.user.avatar_url}/>
+          <p>{this.state.user.loginname}</p>
+        </div>
       </div>
     );
   }
--- src/reducers/account.js
+++ src/reducers/account.js
@@ -1,14 +1,18 @@
 import { LOGIN, LOGOUT } from '../actions/actionTypes'
 
 const initialState = {
-  token: null,
-  loginname: null,
-  logined: false
+  token: localStorage.getItem('token'),
+  loginname: localStorage.getItem('loginname'),
+  logined: localStorage.getItem('logined')
 };
 
 export default function account(state=initialState, action) {
   switch (action.type) {
     case LOGIN:
+      localStorage.setItem('token', action.user.token);
+      localStorage.setItem('loginname', action.user.loginname);
+      localStorage.setItem('logined', action.user.logined);
+
       return Object.assign({}, state,
         {
           token: action.user.token,
@@ -16,6 +20,10 @@ export default function account(state=initialState, action) {
           logined: true
         });
     case LOGOUT:
+      localStorage.setItem('token', null);
+      localStorage.setItem('loginname', null);
+      localStorage.setItem('logined', false);
+
       return Object.assign({}, state,
         {
           token: null,
