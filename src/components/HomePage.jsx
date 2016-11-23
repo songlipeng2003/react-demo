9d3a427244fa2e547a3d488a452c38a2ed164f4d
Thinking Song <123wuming_123@163.com>
2016-11-23 15:11:34 +0800

    add home RefreshControl

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { NavBar, Card, WhiteSpace } from 'antd-mobile';
+import { NavBar, ListView, RefreshControl, ActivityIndicator } from 'antd-mobile';
 import { Link } from 'react-router'
 import { Topic } from '../api';
 
@@ -8,37 +8,95 @@ class HomePage extends React.Component {
   constructor(props){
     super(props);
 
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2
+    });
+
+    this.topics = [];
+    this.page = 1;
+
     this.state = {
-      topics: []
-    };
+      dataSource: dataSource.cloneWithRows(this.topics),
+      refreshing: false
+    }
   }
 
-  componentDidMount(){
+  queryData(clear=false){
+    this.setState({ refreshing: true });
+
+    if(clear){
+      this.page = 1;
+    }
+
     Topic.query().then((response) => {
+      if(clear){
+        this.topics = [];
+      }
+
+      this.topics = this.topics.concat(response.data.data);
+
       this.setState({
-        topics: response.data.data
+        dataSource: this.state.dataSource.cloneWithRows(this.topics),
+        refreshing: false
       });
+
+      this.page ++;
     });
   }
 
-  render() {
+  onRefresh(){
+    this.queryData();
+  }
+
+  onScroll(){
+  }
+
+  componentDidMount(){
+    this.queryData();
+  }
+
+  render(){
+    const separator = (sectionID, rowID) => (
+      <div
+        key={`${sectionID}-${rowID}`}
+        style={{
+          backgroundColor: '#F5F5F9',
+          height: 8,
+          borderTop: '1px solid #ECECED',
+          borderBottom: '1px solid #ECECED'
+        }}
+      />
+    );
+
+    const row = (rowData, sectionID, rowID) => {
+      const topic = this.topics[rowID];
+      return (
+        <div key={rowID}
+          style={{
+            padding: '8px 16px',
+            backgroundColor: 'white'
+          }}
+        >
+          <h3 style={{ padding: 2, marginBottom: 8, borderBottom: '1px solid #F6F6F6' }}>
+            <Link to={`topic/${topic.id}`}>{topic.title}</Link>
+          </h3>
+        </div>
+      );
+    };
+
     return (
       <div>
         <NavBar>首页</NavBar>
 
-        {this.state.topics.map(topic => {
-          return <div key={topic.id}>
-            <WhiteSpace size="lg" />
-            <Card>
-              <Card.Header title={<Link to={`/topic/${topic.id}`}>{topic.title}</Link>} />
-              <Card.Body>
-                <div>发布时间:{topic.create_at}</div>
-              </Card.Body>
-            </Card>
-            <WhiteSpace size="lg" />
-          </div>;
-        })}
+        <ListView dataSource={this.state.dataSource} renderRow={row} renderSeparator={separator}
+          scrollRenderAheadDistance={200} scrollEventThrottle={20} scrollerOptions={{ scrollbars: true }}
+          onScroll={this.onScroll} scrollerOptions={{ scrollbars: true }}
+          style={{height: document.body.scrollHeight}}
+          refreshControl={<RefreshControl distanceToRefresh={80} refreshing={this.state.refreshing} onRefresh={this.onRefresh}
+          />}
+        />
 
+        <ActivityIndicator toast text="正在加载" animating={this.state.refreshing} />
       </div>
     );
   }
